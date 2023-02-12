ac9d61e4e9e4943c43e2bc1f8dd0866fc1de7ae4
thejaggermaister <franlopez@est.ecotec.edu.ec>
2023-02-12 16:38:29 -0500

    file format

--- backend/api/auth/model.go
+++ backend/api/auth/model.go
@@ -8,9 +8,8 @@ import (
 // Account model for the database (serve to auto-generate the table and the columns)
 type Account struct {
 	database.DefaultModel
-	Username string `gorm:"unique" json:"username"`
-	Email    string `gorm:"unique" json:"email"`
-	Password string `json:"password"`
-
+	Username  string     `gorm:"unique" json:"username"`
+	Email     string     `gorm:"unique" json:"email"`
+	Password  string     `json:"password"`
 	LastLogin *time.Time `json:"last_login"`
 }
