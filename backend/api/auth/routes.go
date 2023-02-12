c25319cdae6b4bbfdfa9618b46349231d68c8f5c
thejaggermaister <franlopez@est.ecotec.edu.ec>
2023-02-12 16:37:33 -0500

    routes refactores for auth & getAccount

--- backend/api/auth/routes.go
+++ backend/api/auth/routes.go
@@ -4,6 +4,8 @@ import "github.com/gofiber/fiber/v2"
 
 func Routes(route fiber.Router) {
 	auth := route.Group("/auth")
-
 	auth.Post("/login", Login)
+
+	account := route.Group("/account")
+	account.Get("/:id", GetAccount)
 }
