64803c958278c39f128f42c16bd4d3a0791e2e01
Adrian <adrian.martinsz201@gmail.com>
2023-02-12 22:44:31 -0500

    [ADD]: passport module registered

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
-import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
 })
 export class AuthModule {}
