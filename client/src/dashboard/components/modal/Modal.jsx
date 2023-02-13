a97004e3ce5238b2c90c24f542144fa43a98c59f
Adrian <adrian.martinsz201@gmail.com>
2023-02-13 13:50:29 -0500

    [ADD - Client]: modal

---
+++ client/src/dashboard/components/modal/Modal.jsx
@@ -0,0 +1,32 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import * as React from 'react';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 450,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
+
+const CustomModal = ({ children }) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>{children}</Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default CustomModal;
