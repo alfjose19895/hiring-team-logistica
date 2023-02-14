9fcae012b69fa584504fc982d8e1e6a7f7f499c1
CesarIAndrade <cias.pedernales@gmail.com>
2023-02-14 18:47:54 -0500

    configuring nginx

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 // Pages
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component'; 
+import { RegisterComponent } from './pages/register/register.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 
 // Angular Material
@@ -32,7 +33,7 @@ import { ProductsTableComponent } from './components/products-table/products-tab
     LoginComponent,
     RegisterComponent,
     LayoutComponent,
-    ProductsTableComponent
+    ProductsTableComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,9 +42,9 @@ import { ProductsTableComponent } from './components/products-table/products-tab
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
