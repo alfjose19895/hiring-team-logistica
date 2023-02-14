93274cbbacfde76a9b897d6f0473abe58104c74c
jiork16 <jordan_r_a@hotmail.es>
2023-02-14 17:31:07 -0500

    Categoria lista y formulario

---
+++ Frontend/funiber-client/src/components/ListaCatgoryComponent.vue
@@ -0,0 +1,65 @@
+<template>
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Code</th>
+      <th scope="col">Name</th>
+      <th scope="col">Category</th>
+      <th scope="col">Stok</th>
+    </tr>
+  </thead>
+  <tbody>
+    <template v-for="item in products" :key="item.id">
+      <tr>
+        <th scope="row">1</th>
+        <td>{{ item.code }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.category.__data__.description }}</td>
+        <td>{{ item.stock }}</td>
+      </tr>
+    </template>
+  </tbody>
+</table>
+</template>
+<script>
+  import axios from "axios";
+  const axiosInstance = axios.create({
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "	application/json"
+    }
+  });
+  export default {
+    name: "ListaCatgoryComponent",
+    components: {
+      
+    },
+    data: () => ({
+      form: {
+        Code:     null,
+        Name:     null,
+        Category: null,
+        state:    true,
+        stock:    true
+      },
+      products:[]
+    }),
+    created() {
+    this.getListProducts ();
+  },
+    methods: {
+      submitOrden() {
+        alert("ok")
+      },
+      getListProducts() {
+        axiosInstance.get("http://127.0.0.1:8000/category").then((response) => {
+          this.products=response.data;
+          console.log(this.products);
+          console.log(response.data);
+        });
+      }
+    }
+  }
+  </script>
+  
---
+++ Frontend/funiber-client/src/views/CategoryView.vue
@@ -0,0 +1,56 @@
+<template>
+<NavbarView></NavbarView>
+<form>
+  <div class="row">
+    <div class="col-md-4 mb-3">
+      <label for="validationServer02">Description</label>
+      <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Last name" value="Otto" required>
+    </div>
+  </div>
+  <button class="btn btn-primary" type="submit">Submit form</button>
+</form>
+<ListaProductoComponent></ListaProductoComponent>
+</template>
+<script>
+import NavbarView from "@/views/Layouts/NavbarView.vue";
+import ListaProductoComponent from "@/components/ListaCatgoryComponent.vue";
+import axios from "axios";
+const axiosInstance = axios.create({
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "	application/json"
+  }
+});
+export default {
+  name: "Product",
+  components: {
+    NavbarView,
+    ListaProductoComponent
+  },
+  data: () => ({
+    form: {
+      Code:     null,
+      Name:     null,
+      Category: null,
+      state:    true,
+      stock:    true
+    },
+    category:[]
+  }),
+  created() {
+    this.getListCategory();
+  },
+  methods: {
+    submitOrden() {
+      alert("ok")
+    },
+    getListCategory() {
+      axiosInstance.get("http://127.0.0.1:8000/category").then((response) => {
+        this.category=response.data;
+        console.log(this.category);
+        console.log(response.data);
+      });
+    }
+  }
+}
+</script>
