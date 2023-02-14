ca439f4c4bc5dd5f607b24fa81bdd2f28fe200a2
jiork16 <jordan_r_a@hotmail.es>
2023-02-13 19:50:18 -0500

    front

---
+++ Frontend/funiber-client/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="container">
+      <HelloWorld msg="You did it!" />
+      <nav>
+        <RouterLink to="/home">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Login</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+  <RouterView />
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
---
+++ Frontend/funiber-client/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+import './assets/main.css'
+
+const app = createApp(App)
+app.config.devtools = false
+app.use(router,axios)
+
+app.mount('#app')
