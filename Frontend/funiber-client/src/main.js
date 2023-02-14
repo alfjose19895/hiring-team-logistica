a7ead9aea52c6704d07d44a9604e49a8adbebf95
jiork16 <jordan_r_a@hotmail.es>
2023-02-14 17:23:49 -0500

    Cambios en API y formulario de producto

--- Frontend/funiber-client/src/main.js
+++ Frontend/funiber-client/src/main.js
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import { library} from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
-import './assets/main.css'
-
+library.add(faUserSecret,faEye,faEyeSlash)
 const app = createApp(App)
-app.config.devtools = false
-app.use(router,axios)
+.component('font-awesome-icon', FontAwesomeIcon);
+axios.defaults.baseURL="http://127.0.0.1:8000/";
+axios.defaults.headers.common["Authorization"];
 
+app.use(router,axios)
+app.config.globalProperties.$msg = 'hello'
 app.mount('#app')
