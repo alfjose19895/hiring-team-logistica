522c12ea9b13f146b9df831ca0d1dbeed17baaf1
CesarIAndrade <cias.pedernales@gmail.com>
2023-02-14 13:25:08 -0500

    implementing laravel auditing

--- src/api/app/Models/Product.php
+++ src/api/app/Models/Product.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as AuditableTrait;
 
-class Product extends Model
+class Product extends Model implements Auditable
 {
     use HasFactory;
+    use AuditableTrait;
 
     public function category() {
         return $this->belongsTo(Category::class);
--- src/api/app/Models/Stock.php
+++ src/api/app/Models/Stock.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as AuditableTrait;
 
-class Stock extends Model
+class Stock extends Model implements Auditable
 {
     use HasFactory;
+    use AuditableTrait;
 
     public function product() {
         return $this->belongsTo(Product::class);
